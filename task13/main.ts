40382a258db5ca8515f5bff386014fee09fe6ae2
caisilus <serangor@gmail.com>
2022-12-13 20:14:53 +0300

    Change main to use angle uniform

--- task13/main.ts
+++ task13/main.ts
@@ -6,7 +6,7 @@ import {Drawer} from "../src/drawer";
 import {Transformator} from "../src/transformator";
 import {DrawDataCreator} from "../task12/data_creator";
 import {IndexDrawData} from "../src/draw_data";
-import {Offset, instanceAttributes} from "./instance_attributes";
+import {PlanetAttribute, instanceAttributes} from "./instance_attributes";
 
 class Main {
     gl: WebGL2RenderingContext;
@@ -14,6 +14,7 @@ class Main {
     drawer: Drawer;
     transformator: Transformator;
     cubeData: IndexDrawData;
+    angleUniform: WebGLUniformLocation | null;
 
     constructor(canvas: HTMLCanvasElement) {
         this.gl = this.get_gl(canvas)
@@ -23,13 +24,15 @@ class Main {
         this.transformator = new Transformator(this.gl, this.program);
         const dataCreator = new DrawDataCreator(this.gl);
         this.cubeData = dataCreator.cubeData();
+        
         this.drawer.prepareVertices(this.cubeData.attributeExtractor, this.cubeData.vertices);
         this.drawer.prepareIndices(this.cubeData.indices);
-        console.log("here");
-        this.drawer.prepareInstanceAttributes(Offset, instanceAttributes());
+        this.drawer.prepareInstanceAttributes(PlanetAttribute, instanceAttributes());
+        
         this.gl.enable(this.gl.DEPTH_TEST);
         this.gl.enable(this.gl.CULL_FACE);
         this.transformator.scale(0.3, 0.3, 0.3);
+        this.angleUniform = this.gl.getUniformLocation(this.program, "angle");
         this.configure_loop();
     }
 
@@ -52,9 +55,13 @@ class Main {
     }
 
     update() {
-        this.transformator.rotate([performance.now() / 1000.0 * 60,
-        performance.now() / 2 / 1000.0 * 60,
-        performance.now() / 5 / 1000.0 * 60]);
+        // this.transformator.rotate([performance.now() / 1000.0 * 60,
+        //                            performance.now() / 2 / 1000.0 * 60,
+        //                            performance.now() / 5 / 1000.0 * 60]);
+        // this.transformator.rotateAmongAxis(performance.now() / 1000.0, [0,0,0], [0,1,0]);
+        if (this.angleUniform != null) {
+            this.gl.uniform1f(this.angleUniform, performance.now() / 1000.0);
+        }
         this.drawer.drawIndexInstances(this.cubeData, 3);
         requestAnimationFrame(() => {this.update()});
     }
