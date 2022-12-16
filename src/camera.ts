7f06b96267c057ebe327f02fd324b6883eeb84b5
AndreyKondakovGW <andreykondakov2302@gmail.com>
2022-12-16 16:51:35 +0300

    fix camera

--- src/camera.ts
+++ src/camera.ts
@@ -52,6 +52,11 @@ export class Camera{
         this.cameraDirection[0] = Math.cos(pitch) * Math.cos(yaw);
         this.cameraDirection[1] = Math.sin(pitch);
         this.cameraDirection[2] = Math.cos(pitch) * Math.sin(yaw);
+        this.cameraUp = new Float32Array(3);
+        this.cameraUp[0] = Math.sin(pitch);
+        this.cameraUp[1] =  - Math.cos(pitch) * Math.cos(yaw + Math.PI / 2);
+        this.cameraUp[2] = Math.cos(pitch) * Math.sin(yaw + Math.PI / 2);
+        vec3.normalize(this.cameraUp, this.cameraUp);
         vec3.normalize(this.cameraDirection, this.cameraDirection);
         console.log("dir:" + this.cameraDirection);
     }
@@ -74,10 +79,15 @@ export class Camera{
     }
 
     public moveForward(d: number){
-        this.countDirection();
+        //this.countDirection();
+        console.log("dir:" + this.cameraDirection);
         this.cameraPosition[0] += this.cameraDirection[0] * d;
         this.cameraPosition[1] += this.cameraDirection[1] * d;
         this.cameraPosition[2] += this.cameraDirection[2] * d;
+
+        this.cameraTarget[0] += this.cameraDirection[0] * d;
+        this.cameraTarget[1] += this.cameraDirection[1] * d;
+        this.cameraTarget[2] += this.cameraDirection[2] * d;
         this.setUpView();
         mat4.multiply(this.matCamera, this.matProj, this.matView);
         this.gl.uniformMatrix4fv(this.matCameraUniformLocation, false, this.matCamera);
@@ -85,12 +95,18 @@ export class Camera{
     }
 
     public moveRight(d: number){
-        this.countDirection();
+        /* this.countDirection(); */
         let right = new Float32Array(3);
         vec3.cross(right, this.cameraDirection, this.cameraUp);
+        vec3.normalize(right, right);
+        console.log("right:" + right);
         this.cameraPosition[0] += right[0] * d;
         this.cameraPosition[1] += right[1] * d;
         this.cameraPosition[2] += right[2] * d;
+
+        this.cameraTarget[0] += right[0] * d;
+        this.cameraTarget[1] += right[1] * d;
+        this.cameraTarget[2] += right[2] * d;
         this.setUpView();
         mat4.multiply(this.matCamera, this.matProj, this.matView);
         this.gl.uniformMatrix4fv(this.matCameraUniformLocation, false, this.matCamera);
@@ -98,9 +114,15 @@ export class Camera{
     }
 
     public moveUp(d: number){
+        //this.countDirection();
+        console.log("up:" + this.cameraUp);
         this.cameraPosition[0] += this.cameraUp[0] * d;
         this.cameraPosition[1] += this.cameraUp[1] * d;
         this.cameraPosition[2] += this.cameraUp[2] * d;
+
+        this.cameraTarget[0] += this.cameraUp[0] * d;
+        this.cameraTarget[1] += this.cameraUp[1] * d;
+        this.cameraTarget[2] += this.cameraUp[2] * d;
         this.setUpView();
         mat4.multiply(this.matCamera, this.matProj, this.matView);
         this.gl.uniformMatrix4fv(this.matCameraUniformLocation, false, this.matCamera);
